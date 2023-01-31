83a4c62341919fa71fa96d891c781c2edb8941d3
Y-Deekshith <117278967+Y-Deekshith@users.noreply.github.com>
2023-02-01 00:53:10 +0530

    updated packer module

---
+++ packer/bashami.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ARTIFACT=`packer build -machine-readable packer.json packer-vars.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
